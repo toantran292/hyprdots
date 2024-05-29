7e9898643234f8fb6e95b2e1f2bbd1e1a9e9f936
toantran292 <tranthaitoan998@gmail.com>
2024-05-29 18:55:30 +0700

    .

--- .install/wallpaper.sh
+++ .install/wallpaper.sh
@@ -4,21 +4,21 @@
 echo -e "${GREEN}"
 figlet "Wallpapers"
 echo -e "${NONE}"
-# if [ ! -d ~/wallpaper ]; then
-    # echo "Do you want to download the wallpapers from repository  ?"
-    # echo "If not, the script will install 3 default wallpapers in ~/wallpaper/"
-    # echo ""
-    # if gum confirm "Do you want to download the repository?" ;then
-    #     wget -P ~/Downloads/ ...
-    #     unzip -o ~/Downloads/... -d ~/Downloads/
-    #     if [ ! -d ~/wallpaper/ ]; then
-    #         mkdir ~/wallpaper
-    #     fi
-    #     cp ~/Downloads/wallpaper-main/* ~/wallpaper/
-    #     echo "Wallpapers from the repository installed successfully."
-    # elif [ $? -eq 130 ]; then
-    #     exit 130
-    # else
+if [ ! -d ~/wallpaper ]; then
+    echo "Do you want to download the wallpapers from repository https://gitlab.com/stephan-raabe/wallpaper/ ?"
+    echo "If not, the script will install 3 default wallpapers in ~/wallpaper/"
+    echo ""
+    if gum confirm "Do you want to download the repository?" ;then
+        wget -P ~/Downloads/ https://gitlab.com/stephan-raabe/wallpaper/-/archive/main/wallpaper-main.zip
+        unzip -o ~/Downloads/wallpaper-main.zip -d ~/Downloads/
+        if [ ! -d ~/wallpaper/ ]; then
+            mkdir ~/wallpaper
+        fi
+        cp ~/Downloads/wallpaper-main/* ~/wallpaper/
+        echo "Wallpapers from the repository installed successfully."
+    elif [ $? -eq 130 ]; then
+        exit 130
+    else
         if [ -d ~/wallpaper/ ]; then
             echo "wallpaper folder already exists."
         else
@@ -26,10 +26,10 @@ echo -e "${NONE}"
         fi
         cp wallpapers/* ~/wallpaper
         echo "Default wallpapers installed successfully."
-#     fi
-# else
-#     echo ":: ~/wallpaper folder already exists."
-# fi
+    fi
+else
+    echo ":: ~/wallpaper folder already exists."
+fi
 echo ""
 
 # ------------------------------------------------------
